d16ca3889a636bfc6b4d08167676e25eb562fe11
Gina <1ginahong@gmail.com>
2019-10-13 01:34:49 -0700

    cleaning up the nav

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,8 +10,6 @@ import backer from '../pages/backer';
 import collab from '../pages/collab';
 import collabReq from '../pages/collabReq';
 import ideaPitched from '../pages/ideaPitched';
-import textInput from '../pages/text-input';
-import editorOption from '../pages/editorOption';
 import projectView from '../pages/projectView';
 import { userCreate, projCreate, noteCreate } from '../pages/editorOption';
 import { 
@@ -34,29 +32,10 @@ function App() {
           <div>
             <nav>
               <ul>
-                <li>
-                  <Link to='/hackerBacker'>hackerBacker</Link>
-                </li>
-                <li>
-                  <Link to='/template'>template page</Link>
-                </li>
-                <li>
-                  <Link to='/text-input'>Text input exp</Link>
-                </li>
-                <li>
-                  <Link to='/editorOption'>Text input 2</Link>
-                </li>
-                <li>
-                  <Link to='/projectView'>projectView</Link>
-                </li>
-                <li>
-                  <Link to='/collabReq'>collabReq</Link>
-                </li>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
                 <li><Link to='/hackerBacker'>hackerBacker</Link></li>
                 <li><Link to='/template'>template page</Link></li>
+                <li><Link to='/projectView'>projectView</Link></li>
+                <li><Link to='/collabReq'>collabReq</Link></li>
                 <li><Link to='/projCreate'>Create a Project</Link></li>
                 <li><Link to='/userCreate'>Create a User</Link></li>
                 <li><Link to="/noteCreate">Send a Note (after 'liking' proj)</Link></li>
@@ -67,24 +46,14 @@ function App() {
               </ul>
             </nav>
             <Switch>
-              <Route path='/hackerBacker' component={hackerBacker}>
-              </Route>
-              <Route path='/template' component={template}>
-              </Route>
-              <Route path='/hacker' component={hacker}>
-              </Route>
-              <Route path='/backer' component={backer}>
-              </Route>
-              <Route path='/projCreate' component={projCreate}>
-              </Route>
-              <Route path='/collab' component={collab}>
-              </Route>
-              <Route path='/ideaPitched' component={ideaPitched}>
-              </Route>
-              <Route path='/collabReq' component={collabReq}>
-              </Route>
-              <Route path='/editorOption' component={editorOption}></Route>
-              <Route path='/text-input' component={textInput}></Route>
+              <Route path='/hackerBacker' component={hackerBacker}></Route>
+              <Route path='/template' component={template}></Route>
+              <Route path='/hacker' component={hacker}></Route>
+              <Route path='/backer' component={backer}> </Route>
+              <Route path='/projCreate' component={projCreate}></Route>
+              <Route path='/collab' component={collab}></Route>
+              <Route path='/ideaPitched' component={ideaPitched}></Route>
+              <Route path='/collabReq' component={collabReq}></Route>
               <Route path='/projectView' component={projectView}></Route>
               <Route path='/userCreate' component={userCreate}></Route>
               <Route path='/noteCreate' component={noteCreate}></Route>
