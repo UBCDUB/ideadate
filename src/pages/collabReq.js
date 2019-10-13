80a477252058aba8e9d6bc51991d8ecec907de2c
Stevens Qiu <stevens.qiu@sap.com>
2019-10-13 04:12:11 -0700

    clean collabReq

--- src/pages/collabReq.js
+++ src/pages/collabReq.js
@@ -1,23 +1,31 @@
 // template page
 import React from 'react';
-import requested from "../design_doc/icons/idea_lookout.svg"
+import requested from '../design_doc/icons/idea_lookout.svg';
 
-const templatePage = () => {
+class collabReq extends React.Component {
+  getCollaborationRequestMessage() {
     return (
-        <div>
-            <h2>Collaboration Requested!</h2>
-            <img src={requested} class="img-fluid" alt="Collaboration Requested">
-            </img>
-            <ul>
-                <b>
-                    Your collaboration has been requested! We'll let you know once the project owner has approved your collaboration request.
-            </b>
-            </ul>
-            <ul>
-                <a href="/" class="btn btn-info" role="button">Go back home</a>
-            </ul>
-        </div>
-    )
+      <b>
+        Your collaboration has been requested! We'll let you know once the
+        project owner has approved your collaboration request.
+      </b>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Collaboration Requested!</h2>
+        <img src={requested} class="img-fluid" alt="Collaboration Requested" />
+        <br />
+        {this.getCollaborationRequestMessage()}
+        <br />
+        <a href="/" class="btn btn-info" role="button">
+          Go back home
+        </a>
+      </div>
+    );
+  }
 }
 
-export default templatePage;
+export default collabReq;
