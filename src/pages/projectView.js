e7f7a63981446b06297e72fb6da33d4495b46259
n2r1b <chanbin.lee123@gmail.com>
2019-10-13 04:02:22 -0700

    First time css

--- src/pages/projectView.js
+++ src/pages/projectView.js
@@ -1,15 +1,19 @@
 // template page
 import React from 'react';
 import placeholder from '../design_doc/placeholder.png'
+import './projectView.css';
 
 const templatePage = () => {
     return (
         <div>
-            <img src={placeholder} alt="..." class="img-thumbnail"></img>
-            <h2>Project</h2>
-            <ul>Project Name</ul>
-            <ul>Keyword (Tags)</ul>
-            <ul>Project Description</ul>
+            <div class = "post-container">
+            <div class="image"><img src={placeholder} alt="..." class="img-thumbnail"></img></div>
+            <div class="content-align">
+                <h2>Project</h2>
+                <ul>Project Name</ul>
+                <ul>Keyword (Tags)</ul>
+            </div>
+            </div>
             <p class="lead">Proident eu nisi commodo enim. Deserunt enim duis sunt nostrud anim. Sunt do sit enim veniam nostrud culpa adipisicing do ullamco occaecat et. </p>
         </div>
     )
