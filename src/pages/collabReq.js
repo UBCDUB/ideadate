d537d543a2fb9d210de7e14b08262e34bdeef291
n2r1b <chanbin.lee123@gmail.com>
2019-10-13 05:25:14 -0700

    I wrote bad code and I'm not proud of it

--- src/pages/collabReq.js
+++ src/pages/collabReq.js
@@ -1,31 +1,33 @@
 // template page
 import React from 'react';
 import requested from '../design_doc/icons/idea_lookout.svg';
+import './submitted.css';
 
 class collabReq extends React.Component {
-  getCollaborationRequestMessage() {
-    return (
-      <b>
-        Your collaboration has been requested! We'll let you know once the
-        project owner has approved your collaboration request.
-      </b>
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Collaboration Requested!</h2>
-        <img src={requested} class="img-fluid" alt="Collaboration Requested" />
-        <br />
-        {this.getCollaborationRequestMessage()}
-        <br />
-        <a href="/" class="btn btn-info" role="button">
-          Go back home
-        </a>
+    getCollaborationRequestMessage() {
+        return (
+            <div class="container">
+                Your collaboration has been requested!<br />
+                We'll let you know once the project owner has approved your collaboration request.
       </div>
-    );
-  }
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <center><h2>Collaboration Requested!</h2>
+                        <img src={requested} class="img-fluid" alt="Collaboration Requested"></img>
+                        </center>
+                <br />
+                {this.getCollaborationRequestMessage()}
+                <br />
+                <a href="/" class="btn btn-info" role="button">
+                    Go back home
+        </a>
+            </div>
+        );
+    }
 }
 
 export default collabReq;
--- src/pages/ideaPitched.js
+++ src/pages/ideaPitched.js
@@ -1,25 +1,13 @@
 // template page
 import React from 'react';
 import submitted from "../design_doc/icons/idea_submit.png"
-import styled from 'styled-components'
-
-const image = styled.img`
-    display: flex;
-    position: relative;
-    margin: 10px auto;
-`
-
-const title = styled.h2`
-    text-align: center;
-    position: relative;
-`
 
 const templatePage = () => {
     return (
         <div>
-            <title>Project Submitted!</title>
-            <image src={submitted} class="img-fluid" alt="Project Submitted">
-            </image>
+            <h2>Project Submitted!</h2>
+            <img src={submitted} class="img-fluid" alt="Project Submitted">
+            </img>
             <ul>
                 <b>
                     Your project has been submitted! Sit back and relax while we gather your collaborators.
