8fd0c7b449d2601602cac6ac0a153b9e18d37312
n2r1b <chanbin.lee123@gmail.com>
2019-10-13 08:40:09 -0700

    Center?

--- src/pages/collabReq.js
+++ src/pages/collabReq.js
@@ -1,23 +1,22 @@
 // template page
 import React from 'react';
 import requested from '../design_doc/icons/idea_lookout.svg';
-import './submitted.css';
+import collabStyles from './submitted.scss';
 
 class collabReq extends React.Component {
     getCollaborationRequestMessage() {
         return (
-            <div class="container">
+            <center>
                 Your collaboration has been requested!<br />
                 We'll let you know once the project owner has approved your collaboration request.
-      </div>
+            </center>
         );
     }
 
     render() {
         return (
-            <div>
-
-                <h2>Collaboration Requested!</h2>
+            <div className={collabStyles.container}>
+                <h2 className={collabStyles.h2}>Collaboration Requested!</h2>
                 <div class="container">
                     <div class="image">
                         <img src={requested} class="img-fluid" alt="Collaboration Requested"></img>
--- src/pages/ideaPitched.js
+++ src/pages/ideaPitched.js
@@ -6,18 +6,16 @@ const templatePage = () => {
     return (
         <div>
             <h2>Project Submitted!</h2>
-            <div class="container"><div class="image">
+            
+                <div class="image">
                 <img src={submitted} class="img-fluid" alt="Project Submitted">
                 </img>
-            </div>
-            </div>
-            <ul>
-                <div class="container">
-                    <b>
-                        Your project has been submitted! <br />Sit back and relax while we gather your collaborators.
-            </b>
                 </div>
-            </ul>
+            
+                    <center>
+                        Your project has been submitted! <br />Sit back and relax while we gather your collaborators.
+                        </center>
+                    
             <ul>
                 <a href="/" class="btn btn-info" role="button">Go back home</a>
             </ul>
