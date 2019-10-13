d871a3b4c0256a05ffa5e8fc8c238003d171327a
n2r1b <chanbin.lee123@gmail.com>
2019-10-13 08:46:35 -0700

    Centered more things

--- src/pages/ideaPitched.js
+++ src/pages/ideaPitched.js
@@ -1,21 +1,21 @@
 // template page
 import React from 'react';
 import submitted from "../design_doc/icons/idea_submit.png"
+import collabStyles from './submitted.scss';
 
 const templatePage = () => {
     return (
         <div>
             <h2>Project Submitted!</h2>
-            
-                <div class="image">
-                <img src={submitted} class="img-fluid" alt="Project Submitted">
-                </img>
-                </div>
-            
+
+            <div className={collabStyles.container}>
                     <center>
-                        Your project has been submitted! <br />Sit back and relax while we gather your collaborators.
-                        </center>
-                    
+                    <img src={submitted} class="img-fluid" alt="Project Submitted"></img>
+                    </center>
+            </div>
+            <center>
+            Your project has been submitted! <br />Sit back and relax while we gather your collaborators.
+            </center>
             <ul>
                 <a href="/" class="btn btn-info" role="button">Go back home</a>
             </ul>
