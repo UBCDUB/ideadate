9e17e855ad167548da6b7c61a8b03b7393ac3d80
n2r1b <chanbin.lee123@gmail.com>
2019-10-12 22:48:23 -0700

    Implemented backer and hacker pages + buttons

---
+++ src/pages/backer.js
@@ -0,0 +1,15 @@
+// template page
+import React from 'react';
+
+const backer = () => {
+    return (
+        <div>
+            <h2>Backer</h2>
+            <b>
+                Backer
+            </b>
+        </div>
+    )
+}
+
+export default backer;
---
+++ src/pages/hacker.js
@@ -0,0 +1,15 @@
+// template page
+import React from 'react';
+
+const hacker = () => {
+    return (
+        <div>
+            <h2>Hacker</h2>
+            <b>
+                Hacker
+            </b>
+        </div>
+    )
+}
+
+export default hacker;
